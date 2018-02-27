f258aa10b3bd223b63101c60e146d65b804ae727
Kunal Joshi <kunal.kaustubh@learner.manipal.edu>
2018-02-27 11:59:33 +0530

    added email functions

--- bootcamp.py
+++ bootcamp.py
@@ -1,8 +1,34 @@
 # imports
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-def send_email(to, message):
-    #TODO
-    pass
+def create_server():
+    username = 'bootcamp_manipal@outlook.com'
+    password = input('Enter password: ')
+
+    s = smtplib.SMTP(host='smtp-mail.outlook.com', port=587)
+    s.starttls()
+    s.login(username, password)
+    
+    return s
+
+def send_email(to, body, server):
+    username = 'bootcamp_manipal@outlook.com'
+        
+
+    message = MIMEMultipart()
+    message['From'] = username
+    message['To'] = to
+    message['Subject'] = "TEST"
+    message.attach(MIMEText(body, 'plain'))
+    text = message.as_string()
+    
+    server.sendmail(username, to, text)
+
+def close_server(server):
+    server.close()
+
 
 def read_file(path):
     #TODO
